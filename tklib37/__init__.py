d0b3201da945943b52496a9f673c6dba1873b839
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-02-27 18:52:01 +0100

    fix : version 1.0.8
    added KeyTable import in tklib37.__init__

--- tklib37/__init__.py
+++ tklib37/__init__.py
@@ -3,4 +3,5 @@ from .entries import *
 from .YScrollFrame import YScrollFrame
 from .XScrollFrame import XScrollFrame
 from .Table import Table
+from .KeyTable import KeyTable
 from .SimpleForm import SimpleForm, FormHandler
