fe96cf141fbbd112a8492ea0a87c985a331904b7
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-04-09 03:24:40 +0200

    update of the setup method, and move examples in a proper folder

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
-import setuptools
-import pkg_resources
+from install37 import setup
+from tklib37.__meta__ import __version__
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = list(map(str, pkg_resources.parse_requirements(fh.read())))
-
-setuptools.setup(
-    name="tklib37",
-    version="1.0.11",
-    author="Gabriel Amare",
-    author_email="gabriel.amare.31@gmail.com",
-    description="Tkinter additional widgets library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=requirements,
-    url="https://github.com/GabrielAmare/tklib37",
-    packages=['tklib37'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-)
+if __name__ == "__main__":
+    setup(
+        name="tklib37_",
+        version=__version__,
+        author="Gabriel Amare", 
+        author_email="gabriel.amare.31@gmail.com",
+        description="Tkinter additional widgets library", 
+        url="https://github.com/GabrielAmare/tklib37", 
+        packages=["tklib37"],
+        classifiers=[], 
+        python_requires=">=3.7"
+    )
